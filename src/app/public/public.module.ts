9688e6c131cd56b006f6fcfbe3753c38a2a86540
Akshay Chandwani <akshaychandwani992@gmail.com>
2021-10-05 21:45:16 +0530

    Adding some components to the structure

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -4,16 +4,26 @@ import { CommonModule } from '@angular/common';
 import { PublicRoutingModule } from './public-routing.module';
 import { PublicComponent } from './public.component';
 import { SharedModule } from '../shared/shared.module';
+import { LayoutComponent } from './components/layout/layout.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { ListUsersComponent } from './components/list-users/list-users.component';
 
 
 @NgModule({
   declarations: [
-    PublicComponent
+    PublicComponent,
+    LayoutComponent,
+    ListUsersComponent
   ],
   imports: [
     CommonModule,
     PublicRoutingModule,
+    MatSidenavModule,
     SharedModule
+  ],
+  exports: [
+    PublicComponent,
+    LayoutComponent
   ]
 })
 export class PublicModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatListModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   exports: [
-    NavbarComponent
+    NavbarComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
   ]
 })
 export class SharedModule { }
